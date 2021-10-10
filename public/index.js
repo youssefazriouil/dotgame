15db22b0ce2dccf49a24d9f03461652c3b9a4dad
Glitch (chrome-shoemaker) <none>
2021-10-10 05:32:25 +0000

    💇📚 Checkpoint ./public/index.js:37077094/1657

--- public/index.js
+++ public/index.js
@@ -1,19 +1,26 @@
-import {} from ''
+import { isElementInViewport } from "./helpers.js";
 
-const makeDot = (node) => {
-  const dot = document.createElement("div")
-  dot.classList.add("dot")
-  node.appendChild(dot)
-  
+const makeDot = node => {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  node.appendChild(dot);
 };
 
-const cleanUpDots = (node) => {
-  const allDots = node.getElementsByClassName('dot')
-}
+const cleanUpDots = bodyEl => {
+  const allDots = bodyEl.getElementsByClassName("dot")
+  console.log('allDots',allDots)
+  for(const dot of allDots) {
+    isElementInViewport(dot) && dot.romve()
+  }
+};
+
+const handleTick = bodyEl => {
+  makeDot(bodyEl);
+  cleanUpDots(bodyEl);
+};
 
 const init = () => {
-  const bodyTag = document.getElementsByTagName('body')[0]
+  const bodyTag = document.getElementsByTagName("body")[0];
   // const dotInterval = setInterval(() => handleTick(bodyTag), 1000)
-  
-}
-init()
+};
+init();
