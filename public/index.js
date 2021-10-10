a4f761a452fcc4e1c101e3eb1e5a78be1a605855
Glitch (chrome-shoemaker) <none>
2021-10-10 05:21:25 +0000

    🗺🚄 Checkpoint ./public/index.js:37077094/281 ./views/index.html:37077094/56 ./public/helpers.js:37077094/735

--- public/index.js
+++ public/index.js
@@ -1,15 +1,19 @@
+import {} from ''
+
 const makeDot = (node) => {
-  const dot = document.createElement("div");
+  const dot = document.createElement("div")
   dot.classList.add("dot")
   node.appendChild(dot)
   
 };
 
+const cleanUpDots = (node) => {
+  const allDots = node.getElementsByClassName('dot')
+}
+
 const init = () => {
   const bodyTag = document.getElementsByTagName('body')[0]
-  // const dotInterval = setInterval(() => makeDot(bodyTag), 1000)
+  // const dotInterval = setInterval(() => handleTick(bodyTag), 1000)
   
 }
 init()
-// document.on("load", console.log("blablabla"));
-// console.log('kazkazkAZ')
