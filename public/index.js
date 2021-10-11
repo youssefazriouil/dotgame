38515fa64f2b342593a40c0e8165e4ffe33cf996
Glitch (chrome-shoemaker) <none>
2021-10-11 01:04:44 +0000

    🚅👆 Checkpoint ./public/index.js:37077094/88 ./public/style.css:37077094/51

--- public/index.js
+++ public/index.js
@@ -2,7 +2,7 @@ import { isElementInViewport, getRandomNumber } from "./helpers.js";
 
 const makeDot = mainEl => {
   const dot = document.createElement("div");
-  const size = getRandomNumber(1, 100);
+  const size = getRandomNumber(10, 100);
   dot.style.setProperty('--dot-size', size+'px');
   dot.addEventListener('click', clickDot);
   dot.classList.add("dot");
@@ -28,7 +28,9 @@ const handleTick = mainEl => {
 const init = () => {
   const score = 0;
   const mainEl = document.getElementsByTagName("main")[0];
-  
+  // makeDot(mainEl);
+  // makeDot(mainEl);
+  // makeDot(mainEl);
   // const dotInterval = setInterval(() => handleTick(mainEl), 1000)
 };
 init();
