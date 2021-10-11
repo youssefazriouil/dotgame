578c194692ce8f492c15b948e9d49767cf5befda
Glitch (chrome-shoemaker) <none>
2021-10-11 05:40:31 +0000

    🎪👗 Checkpoint ./public/index.js:37077094/2367 ./public/style.css:37077094/295 ./views/index.html:37077094/16

--- public/index.js
+++ public/index.js
@@ -3,29 +3,31 @@ import { isElementInViewport, getRandomNumber } from "./helpers.js";
 const makeDot = () => {
   const dot = document.createElement("div");
   const size = getRandomNumber(10, 100);
-  dot.style.setProperty('--dot-size', size+'px');
-  dot.addEventListener('click', clickDot);
+  dot.style.setProperty("--dot-size", size + "px");
+  dot.addEventListener("click", clickDot);
   dot.classList.add("dot");
   mainEl.appendChild(dot);
 };
 
-const updateScore = (size) => {
-  const scoreEl = document.getElementById('score');
-  const score = (Math.floor(size/10))%10 || 1;
+const updateScore = size => {
+  const scoreEl = document.getElementById("score");
+  const score = 11 - (Math.floor(size / 10));
   scoreEl.textContent = parseInt(scoreEl.textContent) + parseInt(score);
-}
+};
 
-const clickDot = ({target: dotEl}) => {
-  const size = getComputedStyle(dotEl).width.replace('px', '');
+const clickDot = ({ target: dotEl }) => {
+  const size = getComputedStyle(dotEl).width.replace("px", "");
   updateScore(size);
   dotEl.remove();
-  setTimeout(() => {makeDot, 1000});
-}
+  setTimeout(() => {
+    makeDot, 1000;
+  });
+};
 
 const cleanUpDots = () => {
-  const allDots = mainEl.getElementsByClassName("dot")
-  for(const dot of allDots) {
-    !isElementInViewport(dot) && dot.remove()
+  const allDots = mainEl.getElementsByClassName("dot");
+  for (const dot of allDots) {
+    !isElementInViewport(dot) && dot.remove();
   }
 };
 
@@ -36,8 +38,7 @@ const handleTick = () => {
 
 const init = () => {
   const score = 0;
-  
-
+  makeDot()
   // const dotInterval = setInterval(() => handleTick(mainEl), 1000)
 };
 const mainEl = document.getElementsByTagName("main")[0];
